69021dd4090a467c6d94ef861eeee47cce9dbbb5
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2025-01-18 01:01:35 +0900

    [FEAT] 사용자 스푼 개수 조회 API 구현 완료

---
+++ src/main/java/com/spoony/spoony_server/domain/spoon/service/SpoonService.java
@@ -0,0 +1,32 @@
+package com.spoony.spoony_server.domain.spoon.service;
+
+import com.spoony.spoony_server.common.exception.BusinessException;
+import com.spoony.spoony_server.common.message.SpoonErrorMessage;
+import com.spoony.spoony_server.common.message.UserErrorMessage;
+import com.spoony.spoony_server.domain.spoon.dto.response.SpoonResponseDTO;
+import com.spoony.spoony_server.domain.spoon.entity.SpoonBalanceEntity;
+import com.spoony.spoony_server.domain.spoon.repository.SpoonBalanceRepository;
+import com.spoony.spoony_server.domain.user.entity.UserEntity;
+import com.spoony.spoony_server.domain.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SpoonService {
+
+    private final SpoonBalanceRepository spoonBalanceRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public SpoonResponseDTO getAmountById(Long userId){
+        UserEntity userEntity = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(UserErrorMessage.NOT_FOUND_ERROR));
+
+        SpoonBalanceEntity spoonBalanceEntity = spoonBalanceRepository.findByUser(userEntity)
+                .orElseThrow(() -> new BusinessException(SpoonErrorMessage.USER_NOT_FOUND));
+
+        return new SpoonResponseDTO(spoonBalanceEntity.getAmount());
+    }
+}
