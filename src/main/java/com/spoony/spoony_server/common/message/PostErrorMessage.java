969c318be3a52066e265f1c003023c206039e7f9
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2025-01-20 03:12:15 +0900

    [FEAT] 지도 위치 검색 API 구현 완료

--- src/main/java/com/spoony/spoony_server/common/message/PostErrorMessage.java
+++ src/main/java/com/spoony/spoony_server/common/message/PostErrorMessage.java
@@ -8,7 +8,8 @@ public enum PostErrorMessage implements DefaultErrorMessage {
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
     MISSING_REQUIRED_HEADER(HttpStatus.BAD_REQUEST, "필수 헤더가 누락되었습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다."),
-    ZZIM_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "사용자의 지도 리스트가 비어있습니다.");
+    ZZIM_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "사용자의 지도 리스트가 비어있습니다."),
+    LOCATION_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "해당 지역을 찾을 수 없습니다.");
 
 
     private HttpStatus httpStatus;
