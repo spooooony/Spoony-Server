24d089e65c45f2e30078888b973aebaee5497a8f
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2025-03-07 00:06:38 +0900

    [FIX] Blue Green Check 컨트롤러 수정

--- src/main/java/com/spoony/spoony_server/adapter/test/BlueGreenController.java
+++ src/main/java/com/spoony/spoony_server/adapter/test/BlueGreenController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/deploy")
+@RequestMapping("/api/v1/deploy")
 public class BlueGreenController {
 
     private final Environment environment;
