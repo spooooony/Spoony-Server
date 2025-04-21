39c5d8606327775cf4193b64331cdcb6a44bbf3c
dltnals317 <sumin4101@naver.com>
2025-04-22 03:08:27 +0900

    [FEAT] 유저 생일 응답 타입 변경

--- src/main/java/com/spoony/spoony_server/adapter/dto/user/UserProfileUpdateRequestDTO.java
+++ src/main/java/com/spoony/spoony_server/adapter/dto/user/UserProfileUpdateRequestDTO.java
@@ -1,10 +1,11 @@
 package com.spoony.spoony_server.adapter.dto.user;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public record UserProfileUpdateRequestDTO(String userName,
                                           Long regionId,
                                           String introduction,
-                                          LocalDateTime birth) {
+                                          LocalDate birth) {
 }
 
