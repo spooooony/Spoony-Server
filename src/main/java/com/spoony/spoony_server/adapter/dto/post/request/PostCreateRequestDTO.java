6eaccbd61aa172a21dcfa7ef7e1bbe952bdf0da5
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2025-05-26 21:59:01 +0900

    [HOTFIX] 게시물 title 삭제

--- src/main/java/com/spoony/spoony_server/adapter/dto/post/request/PostCreateRequestDTO.java
+++ src/main/java/com/spoony/spoony_server/adapter/dto/post/request/PostCreateRequestDTO.java
@@ -4,8 +4,7 @@ import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
-public record PostCreateRequestDTO(@NotNull(message = "게시물 제목은 필수 값입니다.") String title,
-                                   @NotNull(message = "게시물 내용은 필수 값입니다.") String description,
+public record PostCreateRequestDTO(@NotNull(message = "게시물 내용은 필수 값입니다.") String description,
                                    @NotNull(message = "가격 대비 만족도는 필수 값입니다.") Double value,
                                    String cons,
                                    @NotNull(message = "장소 이름은 필수 값입니다.") String placeName,
