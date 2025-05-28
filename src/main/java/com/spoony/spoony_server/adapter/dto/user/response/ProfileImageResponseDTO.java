1aeff0bf9a531e2f7ae1d2313e5ee7ec96a01fa4
dltnals317 <sumin4101@naver.com>
2025-05-29 01:13:44 +0900

    [FEAT] 프로필 수저 이미지 변경 + 수저 정보 추가

--- src/main/java/com/spoony/spoony_server/adapter/dto/user/response/ProfileImageResponseDTO.java
+++ src/main/java/com/spoony/spoony_server/adapter/dto/user/response/ProfileImageResponseDTO.java
@@ -4,6 +4,7 @@ import com.spoony.spoony_server.domain.user.ProfileImage;
 
 public record ProfileImageResponseDTO(
         int imageLevel,
+        String spoonName,
         String unlockCondition,
         String imageUrl,
         boolean isUnlocked
@@ -12,6 +13,7 @@ public record ProfileImageResponseDTO(
         String imageUrl = "https://www.spoony.o-r.kr/profile-images/" + profileImage.getImage();
         return new ProfileImageResponseDTO(
                 profileImage.getImageLevel(),
+                profileImage.getSpoonName(),
                 profileImage.getUnlockCondition(),
                 imageUrl,
                 isUnlocked
