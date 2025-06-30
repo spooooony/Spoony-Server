166e85bccbd819dd69f67d131a083eeee9f4b118
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2025-06-30 15:31:26 +0900

    [FEAT] 수저 레벨 네이밍 업데이트

--- src/main/java/com/spoony/spoony_server/domain/user/ProfileImage.java
+++ src/main/java/com/spoony/spoony_server/domain/user/ProfileImage.java
@@ -5,12 +5,12 @@ import lombok.Getter;
 
 @Getter
 public enum ProfileImage {
-    LEVEL_1(1, "프로필 이미지_1 이름 (미정)", "기본 스푼. 스푸니에 오신 걸 환영해요!", "profile1.png",0),
-    LEVEL_2(2,"프로필 이미지_2 이름 (미정)", "내 리뷰가 다른 유저들의 지도에 10번 저장", "profile2.png",10),
-    LEVEL_3(3, "프로필 이미지_3 이름 (미정)","내 리뷰가 다른 유저들의 지도에 30번 저장", "profile3.png",30),
-    LEVEL_4(4, "프로필 이미지_4 이름 (미정)","내 리뷰가 다른 유저들의 지도에 50번 저장", "profile4.png",50),
-    LEVEL_5(5,"프로필 이미지_5 이름 (미정)", "내 리뷰가 다른 유저들의 지도에 70번 저장", "profile5.png",70),
-    LEVEL_6(6, "프로필 이미지_6 이름 (미정)","내 리뷰가 다른 유저들의 지도에 100번 저장", "profile6.png",100);
+    LEVEL_1(1, "신생 스푸니안 (Newborn Spoonian)", "기본 스푼. 스푸니에 오신 걸 환영해요!", "profile1.png",0),
+    LEVEL_2(2,"탐구자 스푸니오 (Explorer Spoonio)", "내 리뷰가 다른 유저들의 지도에 10번 저장", "profile2.png",10),
+    LEVEL_3(3, "학자 스푸니테스 (Scholar Spooniteus)","내 리뷰가 다른 유저들의 지도에 30번 저장", "profile3.png",30),
+    LEVEL_4(4, "대탐험가 스푸니코스 (Great Adventurer Spoonicos)", "내 리뷰가 다른 유저들의 지도에 50번 저장", "profile4.png",50),
+    LEVEL_5(5,"선구자 스푸니엘 (Pioneer Spooniel)", "내 리뷰가 다른 유저들의 지도에 70번 저장", "profile5.png",70),
+    LEVEL_6(6, "대왕 스푸니우스 (Spoonius the Great)", "내 리뷰가 다른 유저들의 지도에 100번 저장", "profile6.png",100);
 
     private final int imageLevel;
     private final String spoonName;
