f60cdc10deb3d0eb350d98646bfebc1c1930b6d0
heogeonho <geonh1016@gmail.com>
2025-07-25 20:31:25 +0900

    [FEAT] admin usecase interface 로직 작성

--- src/main/java/com/spoony/spoony_server/application/port/in/admin/AdminPostUseCase.java
+++ src/main/java/com/spoony/spoony_server/application/port/in/admin/AdminPostUseCase.java
@@ -1,4 +1,15 @@
 package com.spoony.spoony_server.application.port.in.admin;
 
+import com.spoony.spoony_server.adapter.dto.admin.response.ReportedPostListResponseDTO;
+import com.spoony.spoony_server.adapter.dto.admin.response.UserPostListResponseDTO;
+import com.spoony.spoony_server.application.port.command.admin.AdminDeletePostCommand;
+import com.spoony.spoony_server.application.port.command.admin.AdminGetReportedPostsCommand;
+import com.spoony.spoony_server.application.port.command.admin.AdminGetUserPostsCommand;
+
 public interface AdminPostUseCase {
+    ReportedPostListResponseDTO getReportedPosts(AdminGetReportedPostsCommand command);
+
+    UserPostListResponseDTO getPostsByUser(AdminGetUserPostsCommand command);
+
+    void deletePost(AdminDeletePostCommand adminDeletePostCommand);
 }
